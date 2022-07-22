76f300eb17f44af19268fbd5b2d5297b44989b00
KayCodes11 <kayydraws@gmail.com>
2022-07-22 19:40:08 +0530

    learning the design patterns

--- src/reviseJS/arrays/index.js
+++ src/reviseJS/arrays/index.js
@@ -1,9 +1,30 @@
 // forEach, map, filter, reduce, sort, some, every, find, Array.from(), Array.of(), includes
 // https://www.youtube.com/watch?v=DjzlGfhUyAM&list=PLNqp92_EXZBJmAHWnJbVnXsl71hiHCrQh&index=5&ab_channel=JackHerrington
 
+let numbers = [10, 11, 15, 28, 48, 50];
+console.log(numbers.indexOf((4)));
+console.log(numbers.lastIndexOf(""));
+
+for (const index in numbers) {
+  console.log(index);
+  console.log(numbers[index]);
+}
+
+for (const value of numbers) {
+  console.log(value);
+}
+
+numbers.forEach((value, i) => {
+  console.log(i);
+  console.log(value);
+});
+
+
+
+
 // # Reduce/ReduceRight method
 
-let numbers = [10, 20, 25, 45, 55];
+numbers = [10, 20, 25, 45, 55];
 
 let sum = 0;
 
@@ -22,12 +43,12 @@ const n = numbers.reduce((acc, num) => {
 
 console.log(n);
 
-// string formating
+// string formatting
 const languages = ["REACT", "ANGULAR", "VUE", "NODE", "NEXT"];
 const l = languages.reduce((str, lang) => `${str} ${lang}`, "");
 console.log(typeof l, l);
 
-//  reducting to an array
+//  reducing to an array
 let arr = [];
 
 for (const number of numbers) {
@@ -41,7 +62,7 @@ console.log(numbers.reduce((arr, number) => [...arr, number], []));
 const groups = [
   [3, 2],
   [2, 5],
-  [3, 7],
+  [3, 7]
 ];
 
 // [ 2, 2, 2, 5, 5 , 7, 7, 7 ]
@@ -54,7 +75,7 @@ groups.reduce((array, [key, value]) => {
   return array;
 }, []);
 
-// # reducing to an object i.e. frequencly pattern
+// # reducing to an object i.e. frequently pattern
 const lookup = {};
 numbers = [10, 20, 25, 2, 6, 6, 2, 12];
 for (const number of numbers) {
@@ -66,7 +87,7 @@ console.log(
   numbers.reduce(
     (lookup, value) => ({
       ...lookup,
-      [value]: (lookup[value] ?? 0) + 1,
+      [value]: (lookup[value] ?? 0) + 1
     }),
     {}
   )
@@ -76,11 +97,11 @@ console.log(
   numbers.reduce(
     ({ max, min }, value) => ({
       min: Math.min(min, value),
-      max: Math.max(max, value),
+      max: Math.max(max, value)
     }),
     {
       min: Infinity,
-      max: -Infinity,
+      max: -Infinity
     }
   )
 );
